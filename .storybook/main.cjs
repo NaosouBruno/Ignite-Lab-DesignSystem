d86f6a4343de818b96dad021150e42cf0258a56e
Bruno <brunoowap@gmail.com>
2022-10-17 11:26:38 -0300

    incluidos testes

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,7 +12,9 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+    interactionsDebugger: true,
   },
+  staticDirs: ["../public"],
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUTION") {
       config.base = "/Ignite-Lab-DesignSystem/";
